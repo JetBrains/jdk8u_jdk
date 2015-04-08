b9cd683bb89d34b6ec89d602eaf3f8de220dc943
Denis S. Fokin <Denis.Fokin@Gmail.com>
2015-04-08 16:37:24 +0300

    Revert a redundant part of the push

--- src/macosx/native/sun/awt/CInputMethod.m
+++ src/macosx/native/sun/awt/CInputMethod.m
@@ -211,8 +211,7 @@ JNF_COCOA_EXIT(env);
 JNIEXPORT jobject JNICALL Java_sun_lwawt_macosx_CInputMethod_getNativeLocale
 (JNIEnv *env, jobject this)
 {
-    //if (!inputMethodController) return NULL;
-    if (JNI_TRUE) return NULL;
+    if (!inputMethodController) return NULL;
     jobject returnValue = 0;
     __block NSString *isoAbbreviation;
 JNF_COCOA_ENTER(env);
@@ -288,8 +287,7 @@ JNIEXPORT void JNICALL Java_sun_lwawt_macosx_CInputMethodDescriptor_nativeInit
 JNIEXPORT jobject JNICALL Java_sun_lwawt_macosx_CInputMethodDescriptor_nativeGetAvailableLocales
 (JNIEnv *env, jclass klass)
 {
-    //if (!inputMethodController) return NULL;
-    if (JNI_TRUE) return NULL;
+    if (!inputMethodController) return NULL;
     jobject returnValue = 0;
 
     __block NSArray *selectableArray = nil;
