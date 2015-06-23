d4fed4edb14e9356a7d94ae972afa19f8819afbc
Denis S. Fokin <Denis.Fokin@Gmail.com>
2015-06-23 14:49:10 +0300

    Suppress app deactivation

--- src/macosx/classes/sun/lwawt/LWComponentPeer.java
+++ src/macosx/classes/sun/lwawt/LWComponentPeer.java
@@ -962,14 +962,14 @@ public abstract class LWComponentPeer<T extends Component, D extends JComponent>
                 boolean res = parentPeer.requestWindowFocus(cause);
                 // If parent window can be made focused and has been made focused (synchronously)
                 // then we can proceed with children, otherwise we retreat
-                if (!res || !parentWindow.isFocused()) {
+               /* if (!res || !parentWindow.isFocused()) {
                     if (focusLog.isLoggable(PlatformLogger.Level.FINE)) {
                         focusLog.fine("request rejected, res= " + res + ", parentWindow.isFocused()=" +
                                       parentWindow.isFocused());
                     }
                     LWKeyboardFocusManagerPeer.removeLastFocusRequest(getTarget());
                     return false;
-                }
+                }*/
 
                 KeyboardFocusManagerPeer kfmPeer = LWKeyboardFocusManagerPeer.getInstance();
                 Component focusOwner = kfmPeer.getCurrentFocusOwner();
