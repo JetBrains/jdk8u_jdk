863b8e7b605dbc3d2e574fd96a2374bcead5963e
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2015-12-24 16:18:12 +0300

    IDEA-148133 [IU-143.382] [MacOS] IDEA crashes regularly
    Fixed native resource disposal problem by using ref counting
    
    --HG--
    branch : 8u40-verified-fixes

--- src/macosx/classes/sun/java2d/opengl/CGLGraphicsConfig.java
+++ src/macosx/classes/sun/java2d/opengl/CGLGraphicsConfig.java
@@ -41,6 +41,7 @@ import java.awt.image.DataBuffer;
 import java.awt.image.DirectColorModel;
 import java.awt.image.VolatileImage;
 import java.awt.image.WritableRaster;
+import java.util.HashMap;
 
 import sun.awt.CGraphicsConfig;
 import sun.awt.CGraphicsDevice;
@@ -84,6 +85,8 @@ public final class CGLGraphicsConfig extends CGraphicsConfig
                                                 int swapInterval);
     private static native int getOGLCapabilities(long configInfo);
 
+    private static HashMap<Long, Integer> pGCRefCounts = new HashMap<Long, Integer>();
+
     /**
      * Returns GL_MAX_TEXTURE_SIZE from the shared opengl context. Must be
      * called under OGLRQ lock, because this method change current context.
@@ -106,7 +109,7 @@ public final class CGLGraphicsConfig extends CGraphicsConfig
         this.oglCaps = oglCaps;
         this.maxTextureSize = maxTextureSize;
         context = new OGLContext(OGLRenderQueue.getInstance(), this);
-
+        refPConfigInfo(pConfigInfo);
         // add a record to the Disposer so that we destroy the native
         // CGLGraphicsConfigInfo data when this object goes away
         Disposer.addRecord(disposerReferent,
@@ -167,6 +170,30 @@ public final class CGLGraphicsConfig extends CGraphicsConfig
         int oglCaps = getOGLCapabilities(cfginfo);
         ContextCapabilities caps = new OGLContextCaps(oglCaps, ids[0]);
         return new CGLGraphicsConfig(device, pixfmt, cfginfo, textureSize, caps);
+    }
+
+    static void refPConfigInfo(long pConfigInfo) {
+        synchronized (pGCRefCounts) {
+            Integer count = pGCRefCounts.get(pConfigInfo);
+            if (count == null) {
+                count = 1;
+            }
+            else {
+                count++;
+            }
+            pGCRefCounts.put(pConfigInfo, count);
+        }
+    }
+
+    static void deRefPConfigInfo(long pConfigInfo) {
+        synchronized (pGCRefCounts) {
+            Integer count = pGCRefCounts.get(pConfigInfo);
+            if (count != null) {
+                count--;
+                pGCRefCounts.put(pConfigInfo, count);
+                if (count == 0) OGLRenderQueue.disposeGraphicsConfig(pConfigInfo);
+            }
+        }
     }
 
     public static boolean isCGLAvailable() {
@@ -236,7 +263,7 @@ public final class CGLGraphicsConfig extends CGraphicsConfig
         }
         public void dispose() {
             if (pCfgInfo != 0) {
-                OGLRenderQueue.disposeGraphicsConfig(pCfgInfo);
+                deRefPConfigInfo(pCfgInfo);
                 pCfgInfo = 0;
             }
         }
