7ae5bece1b6b9739e3c5ca287a81684d0f6ce341
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2015-12-29 13:41:00 +0300

    IDEA-149882 Issue with fonts in Ubuntu 12.04
    Provided fallback to default font rendering settings if libfontconfig unable to
    match font pattern

--- src/share/native/sun/font/freetypeScaler.c
+++ src/share/native/sun/font/freetypeScaler.c
@@ -488,6 +488,23 @@ Java_sun_font_FreetypeFontScaler_createScalerContextNative(
     return ptr_to_jlong(context);
 }
 
+static int setDefaultScalerSettings(FTScalerContext *context) {
+    if (context->aaType == TEXT_AA_OFF) {
+        context->loadFlags = FT_LOAD_TARGET_MONO;
+    } else if (context->aaType == TEXT_AA_ON) {
+        context->loadFlags = FT_LOAD_TARGET_LIGHT;
+    } else {
+        context->lcdFilter = FT_LCD_FILTER_LIGHT;
+        if (context->aaType == TEXT_AA_LCD_HRGB ||
+            context->aaType == TEXT_AA_LCD_HBGR) {
+            context->loadFlags = FT_LOAD_TARGET_LCD;
+        } else {
+            context->loadFlags = FT_LOAD_TARGET_LCD_V;
+        }
+    }
+    context->renderFlags = FT_LOAD_TARGET_MODE(context->loadFlags);
+}
+
 static int setupFTContext(JNIEnv *env, jobject font2D, FTScalerInfo *scalerInfo, FTScalerContext *context,
                           FT_Bool configureFont) {
     int errCode = 0;
@@ -509,20 +526,7 @@ static int setupFTContext(JNIEnv *env, jobject font2D, FTScalerInfo *scalerInfo,
             context->loadFlags = FT_LOAD_DEFAULT;
 
             if (libFontConfig == NULL) {
-                if (context->aaType == TEXT_AA_OFF) {
-                    context->loadFlags = FT_LOAD_TARGET_MONO;
-                } else if (context->aaType == TEXT_AA_ON) {
-                    context->loadFlags = FT_LOAD_TARGET_LIGHT;
-                } else {
-                    context->lcdFilter = FT_LCD_FILTER_LIGHT;
-                    if (context->aaType == TEXT_AA_LCD_HRGB ||
-                        context->aaType == TEXT_AA_LCD_HBGR) {
-                        context->loadFlags = FT_LOAD_TARGET_LCD;
-                    } else {
-                        context->loadFlags = FT_LOAD_TARGET_LCD_V;
-                    }
-                }
-                context->renderFlags = FT_LOAD_TARGET_MODE(context->loadFlags);
+                setDefaultScalerSettings(context);
                 return 0;
             }
             FcPattern *fcPattern = 0;
@@ -550,7 +554,8 @@ static int setupFTContext(JNIEnv *env, jobject font2D, FTScalerInfo *scalerInfo,
             if (matchResult != FcResultMatch) {
                 (*FcPatternDestroyPtr)(fcPattern);
                 if (logFC) fprintf(stderr, " - NOT FOUND\n");
-                return 1;
+                setDefaultScalerSettings(context);
+                return 0;
             }
             if (logFC) fprintf(stderr, "\nFC_LOG:   ");
             (*FcPatternDestroyPtr)(fcPattern);
