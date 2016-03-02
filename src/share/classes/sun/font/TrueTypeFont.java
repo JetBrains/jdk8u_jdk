ef43df6ec09fa3699e3f960516822da398bcb8e2
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2016-03-02 16:14:10 +0300

    backport original fix for JDK-7162125 from JDK 9

--- src/share/classes/sun/font/TrueTypeFont.java
+++ src/share/classes/sun/font/TrueTypeFont.java
@@ -888,8 +888,8 @@ public class TrueTypeFont extends FileFont {
         }
     }
 
-    /* NB: is it better to move declaration to Font2D? */
-    long getLayoutTableCache() {
+    @Override
+    protected long getLayoutTableCache() {
         try {
           return getScaler().getLayoutTableCache();
         } catch(FontScalerException fe) {
@@ -898,7 +898,7 @@ public class TrueTypeFont extends FileFont {
     }
 
     @Override
-    byte[] getTableBytes(int tag) {
+    protected byte[] getTableBytes(int tag) {
         ByteBuffer buffer = getTableBuffer(tag);
         if (buffer == null) {
             return null;
