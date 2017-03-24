6e395341c393eb5b8fdae6cd49b351df51baa051
Denis Fokin <denis.fokin@jetbrains.com>
2017-03-24 16:32:09 +0300

    Fallback mode, compatible with OpenJDK key event processing
    (cherry picked from commit 33704a6)

--- src/macosx/native/sun/awt/AWTView.m
+++ src/macosx/native/sun/awt/AWTView.m
@@ -645,8 +645,15 @@ AWT_ASSERT_APPKIT_THREAD;
         }
     }
 
+    jstring oldCharacters = NULL;
+    jstring oldCharactersIgnoringModifiers = NULL;
+    if ([event type] != NSFlagsChanged) {
+        oldCharacters = JNFNSToJavaString(env, [event characters]);
+        oldCharactersIgnoringModifiers = JNFNSToJavaString(env, [event charactersIgnoringModifiers]);
+    }
     static JNF_CLASS_CACHE(jc_NSEvent, "sun/lwawt/macosx/NSEvent");
-    static JNF_CTOR_CACHE(jctor_NSEvent, jc_NSEvent, "(IISLjava/lang/String;Ljava/lang/String;Ljava/lang/String;ZI)V");
+    static JNF_CTOR_CACHE(jctor_NSEvent, jc_NSEvent,
+    "(IISLjava/lang/String;Ljava/lang/String;Ljava/lang/String;ZILjava/lang/String;Ljava/lang/String;)V");
     jobject jevent = JNFNewObject(env, jctor_NSEvent,
                                   [event type],
                                   [event modifierFlags],
@@ -655,7 +662,9 @@ AWT_ASSERT_APPKIT_THREAD;
                                   charactersIgnoringModifiers,
                                   charactersIgnoringModifiersAndShift,
                                   isDeadKeyPressed,
-                                  javaDeadKeyCode
+                                  javaDeadKeyCode,
+                                  oldCharacters,
+                                  oldCharactersIgnoringModifiers
                                   );
 
     static JNF_CLASS_CACHE(jc_PlatformView, "sun/lwawt/macosx/CPlatformView");
