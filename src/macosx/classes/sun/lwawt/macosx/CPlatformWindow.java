117e72d07f49aad9848aaf2f62456e00253841c1
Anton Tarasov <anton.tarasov@jetbrains.com>
2017-05-23 17:42:17 +0300

    JRE-373 [macos] nativeCreateNSWindow deadlocks with a11y
    (cherry picked from commit 72c77a9)

--- src/macosx/classes/sun/lwawt/macosx/CPlatformWindow.java
+++ src/macosx/classes/sun/lwawt/macosx/CPlatformWindow.java
@@ -35,6 +35,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.Callable;
 
 import javax.swing.*;
 
@@ -249,8 +250,10 @@ public class CPlatformWindow extends CFRetainedResource implements PlatformWindo
         } else {
             bounds = _peer.constrainBounds(_target.getBounds());
         }
-        final long nativeWindowPtr = nativeCreateNSWindow(contentView.getAWTView(),
-                ownerPtr, styleBits, bounds.x, bounds.y, bounds.width, bounds.height);
+        long nativeWindowPtr = LWCToolkit.performOnMainThreadWaiting(() ->
+                nativeCreateNSWindow(contentView.getAWTView(),
+                                     ownerPtr, styleBits,
+                                     bounds.x, bounds.y, bounds.width, bounds.height));
         setPtr(nativeWindowPtr);
 
         if (target instanceof javax.swing.RootPaneContainer) {
