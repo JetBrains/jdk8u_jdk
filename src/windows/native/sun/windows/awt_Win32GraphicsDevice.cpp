73914a7cefc942f7d6f0aac9f6aae69e9a77cd70
Anton Tarasov <anton.tarasov@jetbrains.com>
2017-07-20 16:11:03 +0300

    JRE-427 HighDPI Scaling not on all screens
    review: JR-CR-179
    
    (cherry picked from commit 1348f43)

--- src/windows/native/sun/windows/awt_Win32GraphicsDevice.cpp
+++ src/windows/native/sun/windows/awt_Win32GraphicsDevice.cpp
@@ -696,7 +696,7 @@ void AwtWin32GraphicsDevice::ScaleDownDPoint(POINT *pt)
     pt->y = device == NULL ? pt->y : device->ScaleDownDY(pt->y);
 }
 
-void AwtWin32GraphicsDevice::InitDesktopScales(bool fractionalScaleEnabled)
+void AwtWin32GraphicsDevice::InitDesktopScales()
 {
     unsigned x = 0;
     unsigned y = 0;
@@ -752,8 +752,7 @@ void AwtWin32GraphicsDevice::InitDesktopScales(bool fractionalScaleEnabled)
 
     float scaleX = dpiX / 96;
     float scaleY = dpiY / 96;
-    bool isFractScale = (scaleX != (int)scaleX || scaleY != (int)scaleY);
-    if (scaleX > 0 && scaleY > 0 && (fractionalScaleEnabled || !isFractScale)) {
+    if (scaleX > 0 && scaleY > 0) {
         SetScale(scaleX, scaleY);
     }
 }
@@ -1534,12 +1533,12 @@ JNIEXPORT jfloat JNICALL
 */
 JNIEXPORT void JNICALL
 Java_sun_awt_Win32GraphicsDevice_initNativeScale
-(JNIEnv *env, jobject thisPtr, jint screen, jboolean fractionalScaleEnabled)
+(JNIEnv *env, jobject thisPtr, jint screen)
 {
     Devices::InstanceAccess devices;
     AwtWin32GraphicsDevice *device = devices->GetDevice(screen);
 
     if (device != NULL) {
-        device->InitDesktopScales((bool)fractionalScaleEnabled);
+        device->InitDesktopScales();
     }
 }
