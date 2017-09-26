a2917a489382ec2e6ed3fe02479b6f3227b0cb7e
Anton Tarasov <anton.tarasov@jetbrains.com>
2017-09-26 16:13:52 +0300

    JRE-310 [backported] JDK-8069348: SunGraphics2D.copyArea() does not properly work for scaled graphics in D3D

--- src/windows/classes/sun/java2d/windows/GDIWindowSurfaceData.java
+++ src/windows/classes/sun/java2d/windows/GDIWindowSurfaceData.java
@@ -311,13 +311,10 @@ public class GDIWindowSurfaceData extends SurfaceData {
                             int x, int y, int w, int h, int dx, int dy)
     {
         CompositeType comptype = sg2d.imageComp;
-        if (sg2d.transformState < sg2d.TRANSFORM_TRANSLATESCALE &&
-            sg2d.clipState != sg2d.CLIP_SHAPE &&
+        if (sg2d.clipState != SunGraphics2D.CLIP_SHAPE &&
             (CompositeType.SrcOverNoEa.equals(comptype) ||
              CompositeType.SrcNoEa.equals(comptype)))
         {
-            x += sg2d.transX;
-            y += sg2d.transY;
             int dstx1 = x + dx;
             int dsty1 = y + dy;
             int dstx2 = dstx1 + w;
