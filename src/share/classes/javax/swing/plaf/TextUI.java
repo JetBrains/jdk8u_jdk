d3fd54c5a478cdd22850aedd7012994e0a21d114
Anton Tarasov <anton.tarasov@jetbrains.com>
2017-10-12 17:45:27 +0300

    JRE-310 TextUI changes backported from JDK-8132119
    (cherry picked from commit 56d234f)

--- src/share/classes/javax/swing/plaf/TextUI.java
+++ src/share/classes/javax/swing/plaf/TextUI.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -24,11 +24,8 @@
  */
 package javax.swing.plaf;
 
-import javax.swing.Action;
-import javax.swing.BoundedRangeModel;
 import java.awt.Point;
 import java.awt.Rectangle;
-import java.awt.Insets;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import javax.swing.text.*;
@@ -44,8 +41,9 @@ public abstract class TextUI extends ComponentUI
      * Converts the given location in the model to a place in
      * the view coordinate system.
      *
+     * @param t the text component for which this UI is installed
      * @param pos  the local location in the model to translate &gt;= 0
-     * @return the coordinates as a rectangle
+     * @return the coordinates as a {@code Rectangle}
      * @exception BadLocationException  if the given position does not
      *   represent a valid location in the associated document
      *
@@ -59,8 +57,10 @@ public abstract class TextUI extends ComponentUI
      * Converts the given location in the model to a place in
      * the view coordinate system.
      *
+     * @param t the text component for which this UI is installed
      * @param pos  the local location in the model to translate &gt;= 0
-     * @return the coordinates as a rectangle
+     * @param bias the bias for the position
+     * @return the coordinates as a {@code Rectangle}
      * @exception BadLocationException  if the given position does not
      *   represent a valid location in the associated document
      *
@@ -74,17 +74,28 @@ public abstract class TextUI extends ComponentUI
      * Converts the given location in the model to a place in
      * the view coordinate system.
      *
-     * @param pos  the local location in the model to translate &gt;= 0
-     * @return the coordinates as a rectangle
+     * @implSpec This implementation calls
+     * {@link #modelToView(JTextComponent, int, Position.Bias) modelToView(t, pos, bias)}.
+     *
+     * @param t the text component for which this UI is installed
+     * @param pos  the local location in the model to translate {@code >= 0}
+     * @param bias the bias for the position
+     * @return the coordinates as a {@code Rectangle2D}
      * @exception BadLocationException  if the given position does not
-     *   represent a valid location in the associated document
+     *            represent a valid location in the associated document
+     *
+     * @since 9
      */
-    public abstract Rectangle2D modelToView2D(JTextComponent tc, int pos, Position.Bias bias) throws BadLocationException;
+    public Rectangle2D modelToView2D(JTextComponent t, int pos, Position.Bias bias)
+            throws BadLocationException {
+        return modelToView(t, pos, bias);
+    }
 
     /**
      * Converts the given place in the view coordinate system
      * to the nearest representative location in the model.
      *
+     * @param t the text component for which this UI is installed
      * @param pt  the location in the view to translate.  This
      *   should be in the same coordinate system as the mouse
      *   events.
@@ -100,6 +111,7 @@ public abstract class TextUI extends ComponentUI
      * Provides a mapping from the view coordinate space to the logical
      * coordinate space of the model.
      *
+     * @param t the text component for which this UI is installed
      * @param pt the location in the view to translate.
      *           This should be in the same coordinate system
      *           as the mouse events.
@@ -122,18 +134,28 @@ public abstract class TextUI extends ComponentUI
      * Provides a mapping from the view coordinate space to the logical
      * coordinate space of the model.
      *
+     * @implSpec This implementation calls
+     * {@link #viewToModel(JTextComponent, Point, Position.Bias[])
+     * viewToModel(t, new Point((int) pt.getX(), (int) pt.getY()),
+     *             biasReturn)}.
+     *
+     * @param t the text component for which this UI is installed
      * @param pt the location in the view to translate.
-     *           This should be in the same coordinate system
-     *           as the mouse events.
      * @param biasReturn
      *           filled in by this method to indicate whether
      *           the point given is closer to the previous or the next
      *           character in the model
      *
      * @return the location within the model that best represents the
-     *         given point in the view &gt;= 0
+     *         given point in the view {@code >= 0}
+     *
+     * @since 9
      */
-    public abstract int viewToModel2D(JTextComponent tc, Point2D pt, Position.Bias[] biasReturn);
+    public int viewToModel2D(JTextComponent t, Point2D pt,
+                             Position.Bias[] biasReturn) {
+        return viewToModel(t, new Point((int) pt.getX(), (int) pt.getY()),
+                biasReturn);
+    }
 
     /**
      * Provides a way to determine the next visually represented model
@@ -151,18 +173,19 @@ public abstract class TextUI extends ComponentUI
      * @param biasRet an array to contain the bias for the returned position
      * @return the location within the model that best represents the next
      *  location visual position
-     * @exception BadLocationException
+     * @exception BadLocationException for a bad location within a document model
      * @exception IllegalArgumentException for an invalid direction
      */
     public abstract int getNextVisualPositionFrom(JTextComponent t,
-                         int pos, Position.Bias b,
-                         int direction, Position.Bias[] biasRet)
-                         throws BadLocationException;
+                                                  int pos, Position.Bias b,
+                                                  int direction, Position.Bias[] biasRet)
+            throws BadLocationException;
 
     /**
      * Causes the portion of the view responsible for the
      * given part of the model to be repainted.
      *
+     * @param t the text component for which this UI is installed
      * @param p0 the beginning of the range &gt;= 0
      * @param p1 the end of the range &gt;= p0
      */
@@ -172,8 +195,13 @@ public abstract class TextUI extends ComponentUI
      * Causes the portion of the view responsible for the
      * given part of the model to be repainted.
      *
+     * @param t the text component for which this UI is installed
      * @param p0 the beginning of the range &gt;= 0
      * @param p1 the end of the range &gt;= p0
+     * @param firstBias the bias of the first character position, toward the
+     *        previous character or the next character
+     * @param secondBias the bias of the second character position, toward the
+     *        previous character or the next character
      */
     public abstract void damageRange(JTextComponent t, int p0, int p1,
                                      Position.Bias firstBias,
@@ -185,6 +213,7 @@ public abstract class TextUI extends ComponentUI
      * things like the commands available, stream readers and
      * writers, etc.
      *
+     * @param t the text component for which this UI is installed
      * @return the editor kit binding
      */
     public abstract EditorKit getEditorKit(JTextComponent t);
@@ -195,13 +224,18 @@ public abstract class TextUI extends ComponentUI
      * can be traversed to determine how the model is being
      * represented spatially.
      *
-     * @return the view
+     * @param t the text component for which this UI is installed
+     * @return a {@code View} with the allocation of the associated
+     *         text component
      */
     public abstract View getRootView(JTextComponent t);
 
     /**
      * Returns the string to be used as the tooltip at the passed in location.
      *
+     * @param t  the text component for which this UI is installed
+     * @param pt a {@code Point} specifying location for which to get a tooltip
+     * @return a {@code String} containing the tooltip
      * @see javax.swing.text.JTextComponent#getToolTipText
      * @since 1.4
      *
@@ -211,5 +245,23 @@ public abstract class TextUI extends ComponentUI
     @Deprecated
     public String getToolTipText(JTextComponent t, Point pt) {
         return null;
+    }
+    /**
+     * Returns the string to be used as the tooltip at the passed in location.
+     *
+     * @implSpec This implementation calls
+     * {@link #getToolTipText(JTextComponent, Point)
+     *     getToolTipText(t, new Point((int) pt.getX(), (int) pt.getY())))}.
+     *
+     * @param t  the text component for which this UI is installed
+     * @param pt a {@code Point} specifying location for which to get a tooltip
+     * @return a {@code String} containing the tooltip
+     *
+     * @see javax.swing.text.JTextComponent#getToolTipText
+     *
+     * @since 9
+     */
+    public String getToolTipText2D(JTextComponent t, Point2D pt) {
+        return getToolTipText(t, new Point((int) pt.getX(), (int) pt.getY()));
     }
 }
