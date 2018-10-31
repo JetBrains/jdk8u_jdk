e6cbc2bd2fac108dcb3dad8bf7120221e3980f2b
Alexey Ushakov <Alexey.Ushakov@jetbrains.com>
2018-10-31 22:02:30 +0300

    JRE-1008 Do not use LCD shader on macOS 10.14+ in font rendering
    Do not enable CAPS_EXT_LCD_SHADER if it was already disabled

--- src/macosx/native/sun/java2d/opengl/CGLGraphicsConfig.m
+++ src/macosx/native/sun/java2d/opengl/CGLGraphicsConfig.m
@@ -345,12 +345,12 @@ Java_sun_java2d_opengl_CGLGraphicsConfig_getCGLConfigInfo
         if (fontSmoothingDisabled) {
             J2dRlsTraceLn(J2D_TRACE_INFO,
                           "LCD_SHADER: disabled via macOS settings");
-            caps ^= CAPS_EXT_LCD_SHADER;
+            caps &= ~CAPS_EXT_LCD_SHADER;
         }
     } else if (IS_OSX_GT10_13) {
         J2dRlsTraceLn(J2D_TRACE_INFO,
                       "LCD_SHADER: disabled on macOS 10.14+ by default");
-        caps ^= CAPS_EXT_LCD_SHADER;
+        caps &= ~CAPS_EXT_LCD_SHADER;
     }
 
     GLint value = 0;
