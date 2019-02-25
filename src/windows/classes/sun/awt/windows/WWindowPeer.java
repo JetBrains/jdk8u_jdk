b9ad2305234586eb86b45444d6e436e0ddf48eab
phh <unknown>
2019-02-25 09:40:04 +0000

    8207070: Webstart app popup on wrong screen in a one-screen setup changing to multi-monitor Summary: Run updates in a thread pool rather than on the display thread Reviewed-by: prr

--- src/windows/classes/sun/awt/windows/WWindowPeer.java
+++ src/windows/classes/sun/awt/windows/WWindowPeer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1996, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1996, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -549,13 +549,7 @@ public class WWindowPeer extends WPanelPeer implements WindowPeer,
      * Called from native code when we have been dragged onto another screen.
      */
     void draggedToNewScreen() {
-        SunToolkit.executeOnEventHandlerThread((Component)target,new Runnable()
-        {
-            @Override
-            public void run() {
-                displayChanged();
-            }
-        });
+        displayChanged();
     }
 
     public void updateGC() {
@@ -616,8 +610,12 @@ public class WWindowPeer extends WPanelPeer implements WindowPeer,
      */
     @Override
     public void displayChanged() {
+<<<<<<< HEAD
         updateGC();
         adjustBoundsOnDPIChange();
+=======
+        SunToolkit.executeOnEventHandlerThread(target, this::updateGC);
+>>>>>>> 99f7f53... 8207070: Webstart app popup on wrong screen in a one-screen setup changing to multi-monitor
     }
 
     private native void adjustBoundsOnDPIChange();
