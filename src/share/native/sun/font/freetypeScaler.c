26a5c40c4c0a9a8c337895575029743174ded505
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2019-05-29 05:04:46 +0700

    JBR-1555  After update from 2019.1.2 to 2019.1.3 Chinese glyphs look blurry
    Backport of JBR-1545 (additional fix for JBR-1113 in jdk8u):
    
    JBR-1113  java/awt/FontClass/GlyphRotationTest.java: java.lang.RuntimeException: cnt 1 = 13 cnt 2 = 13
    
    (cherry picked from commit 8badbba584be6766a8c977a0b60069c45944a67b)

--- src/share/native/sun/font/freetypeScaler.c
+++ src/share/native/sun/font/freetypeScaler.c
@@ -590,6 +590,20 @@ Java_sun_font_FreetypeFontScaler_createScalerContextNative(
     context->doBold = (boldness != 1.0);
     context->doItalize = (italic != 0);
 
+    /* freetype is very keen to use embedded bitmaps, even if it knows
+     * there is a rotation or you asked for antialiasing.
+     * In the rendering path we will check useSBits and disable
+     * bitmaps unless it is set. And here we set it only if none
+     * of the conditions invalidate using it.
+     * Note that we allow embedded bitmaps for the LCD case.
+     */
+    if ((aa != TEXT_AA_ON) && (fm != TEXT_FM_ON) &&
+        !context->doBold && !context->doItalize &&
+        (context->transform.yx == 0) && (context->transform.xy == 0))
+    {
+        context->useSbits = 1;
+    }
+
     return ptr_to_jlong(context);
 }
 
